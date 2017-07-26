3ab1bb9647950e98dd528b60d9c98959e37ef6fa
yicold <yicold1q84@icloud.com>
2017-07-26 17:46:33 +0800

    库卡停车

---
+++ coolcar/files/g8后台思维导图/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,w,E,w,F,G,H,null,I,J,K,L,M,N,O,J),P,_(Q,_(),R,_()),S,_(T,_(U,V,W,[_(U,X,Y,g,Z,[_(ba,bb,U,bc,bd,_(be,bf,bg,_(bh,bi,bj,bk,bl,[]),bm,g),bn,bo)])])),bp,_(bq,[])),br,_(),bs,_());}; 
+var b="url",c="g8后台思维导图.html",d="generationDate",e=new Date(1471676045975.55),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="page",l="packageId",m="4ceffce1898e494eb85af684013c3825",n="type",o="Axure:Page",p="name",q="G8后台思维导图",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="near",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="a",R="b",S="interactionMap",T="onLoad",U="description",V="OnPageLoad",W="cases",X="打开思维导图",Y="isNewIfGroup",Z="actions",ba="action",bb="linkWindow",bc="Open other/coolcar.swf in Current Window",bd="target",be="targetType",bf="webUrl",bg="urlLiteral",bh="exprType",bi="stringLiteral",bj="value",bk="other/coolcar.swf",bl="stos",bm="includeVariables",bn="linkType",bo="current",bp="diagram",bq="objects",br="masters",bs="objectPaths";
+return _creator();
+})());
